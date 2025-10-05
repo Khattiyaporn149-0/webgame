f3b64a7f6596cd1fe0b5a10240bc4edddba5bb9b
Khattiyaporn149-0 <oumssw@gmail.com>
2025-10-05 16:09:40 +0700

    test

--- server/index.js
+++ server/index.js
@@ -72,3 +72,5 @@ function generateRoomCode() {
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 // End of server/index.js
+
+//test
