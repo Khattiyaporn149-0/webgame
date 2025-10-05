daaa8753d94b2b9e17b724ba245c9b955779b798
Khattiyaporn149-0 <oumssw@gmail.com>
2025-10-05 15:26:21 +0700

    fix : port ip

--- server/index.js
+++ server/index.js
@@ -42,5 +42,7 @@ function randomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
 }
 
-server.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
